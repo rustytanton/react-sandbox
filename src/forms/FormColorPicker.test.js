00df4398921f37cb230ca9959302554b271499d2
Rusty Tanton <rusty.tanton@gmail.com>
2020-04-17 16:44:21 -0400

    remove the failing test to make sure travis passes again

--- src/forms/FormColorPicker.test.js
+++ src/forms/FormColorPicker.test.js
@@ -2,10 +2,6 @@ import React from 'react';
 import { render, getAllByText } from '@testing-library/react';
 import FormColorPicker from './FormColorPicker';
 
-it('intentionally failing to test to see if build fails and status changes', () => {
-    expect(true).toBe(false)
-})
-
 it('should call the onPick method with the correct argument when item is selected', () => {
     expect(true).toBe(true)
 })
