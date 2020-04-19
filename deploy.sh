482b20b7ccfd1faa74bc068f9cbb02396525a14b
Rusty Tanton <rusty.tanton@gmail.com>
2020-04-19 11:36:07 -0400

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,8 @@
 # $WEBHOST_ROOT
 # $WEBHOST_PUBLIC_KEY
 
-WEBHOST_ROOT_BACKUP=$WEBHOST_ROOT.bak
-
 echo 'Backing up last deployed version of the site...'
-ssh $WEBHOST_USER@$WEBHOST_DOMAIN "rm -rf $WEBHOST_ROOT_BACKUP && cp -R $WEBHOST_ROOT $WEBHOST_ROOT_BACKUP"
+ssh $WEBHOST_USER@$WEBHOST_DOMAIN "rm -rf $WEBHOST_ROOT_BACKUP && cp -R $WEBHOST_ROOT $WEBHOST_ROOT.bak && rm -rf $WEBHOST_ROOT/!(cgi-bin)"
 
 echo 'Deploying latest files...'
 # scp -rp $TRAVIS_BUILD_DIR/build/* $WEBHOST_USER@$WEBHOST_DOMAIN:$WEBHOST_ROOT/
