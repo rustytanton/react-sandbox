32feed64c79094580cc8025ad67b4736fd55f0db
Rusty Tanton <rusty.tanton@gmail.com>
2020-04-24 17:27:45 -0400

    update contentful-tokens.sh

--- contentful-tokens.sh
+++ contentful-tokens.sh
@@ -1,4 +1,4 @@
 # keys populated by Travis CI
-cat << EOF > /src/contentful-tokens.json
-{"space": $CONTENTFUL_SPACE, "accessToken": $CONTENTFUL_ACCESS_TOKEN}
+cat << EOF > ./src/contentful-tokens.json
+{"space": "$CONTENTFUL_SPACE", "accessToken": "$CONTENTFUL_ACCESS_TOKEN"}
 EOF
